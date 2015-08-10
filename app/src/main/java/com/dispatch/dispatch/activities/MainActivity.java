8e89d4370ddff41a53a1fa0f3a9cf882f5cb74b5
Daniel <dan_pc@live.com>
2015-08-10 14:35:22 -0400

    Added parse dependencies to the project

--- app/src/main/java/com/dispatch/dispatch/activities/MainActivity.java
+++ app/src/main/java/com/dispatch/dispatch/activities/MainActivity.java
@@ -5,8 +5,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.dispatch.dispatch.CrimeTypeDialog;
+import com.dispatch.dispatch.DispatchHandler;
 import com.dispatch.dispatch.R;
 import com.dispatch.dispatch.models.Crime;
 import com.dispatch.dispatch.utils.APIClient;
@@ -22,5 +26,22 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button helpMe = (Button) findViewById(R.id.btn_helpMe);
+        Button seeCrime = (Button) findViewById(R.id.btn_seeCrime);
+
+        helpMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show dialog asking what kind of crime occurred
+                new CrimeTypeDialog(MainActivity.this, new CrimeTypeDialog.CrimeTypeDialogListener() {
+                    @Override
+                    public void sendHelp(Crime.Type type) {
+                        // The user selected a crime
+                        new DispatchHandler(MainActivity.this).dispatch(type);
+                    }
+                });
+            }
+        });
     }
 }
